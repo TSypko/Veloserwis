7592758e16b11e06c10f3aab84818548033a0005
Tomasz Sypko <tsypko@gmail.com>
2020-08-17 20:57:46 +0200

    Components added

--- src/App.js
+++ src/App.js
@@ -5,21 +5,26 @@ import LogoContainer from "./components/LogoContainer/index"
 import Navigation from "./components/Navigation"
 import NavigationMenu from "./components/NavigationMenu"
 import Main from "./components/Main"
+import Section from "./components/Section"
+import Hero from "./components/Hero"
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <Wrapper isRed={true}>
-      <Navigation>
-        <LogoContainer />
-        <NavigationMenu />
-      </Navigation>
-      </Wrapper>
       <Wrapper>
-        <Main>
-        </Main>
+        <Navigation>
+          <LogoContainer />
+          <NavigationMenu />
+        </Navigation>
       </Wrapper>
+      <Main>
+        <Section
+        body={
+          <Hero/>
+        }
+        />
+      </Main>
     </>
   );
 }
