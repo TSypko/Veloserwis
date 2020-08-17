da5b4cabc3f70297da29a0f6c828bd2b366a1055
Tomasz Sypko <tsypko@gmail.com>
2020-08-17 18:38:48 +0200

    NavigationMenu Created

---
+++ src/components/NavigationMenu.js
@@ -0,0 +1,48 @@
+import React from "react";
+import styled from "styled-components";
+
+const List = styled.ul`
+    list-style-type: none;
+    display: flex;
+    justify-content: flex-end;
+    padding: 10px 50px;
+`;
+
+const ListItem = styled.li`
+    padding: 0px 20px;
+    letter-spacing: 2px;
+    font-weight: 300;
+`;
+const ListLink = styled.a`
+    text-decoration: none;
+    color: inherit;
+    padding: 5px;
+    &:hover {
+        color: #FFF;
+        border-bottom: 1px solid white;
+    }
+    &:active {
+        color: #222;
+        font-weight: 500;
+        border-bottom: 1px solid #222;
+    }
+`;
+
+const NavigationList = () => (
+    <List>
+        <ListItem>
+            <ListLink href="#">start</ListLink>
+        </ListItem>
+        <ListItem>
+            <ListLink href="#">o nas</ListLink>
+        </ListItem>
+        <ListItem>
+            <ListLink href="#">cennik</ListLink>
+        </ListItem>
+        <ListItem>
+            <ListLink href="#">kontakt</ListLink>
+        </ListItem>
+    </List>
+);
+
+export default NavigationList;
