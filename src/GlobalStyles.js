350bccb2ea79899343559ffb0d29259d31c0aa4f
Tomasz Sypko <tsypko@gmail.com>
2020-08-17 18:37:29 +0200

    GlobalStyles updated

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -4,6 +4,8 @@ export const GlobalStyles = createGlobalStyle`
 
   html {
     box-sizing: border-box;
+    --primaryColor: #FF5151;
+    --secondaryColor: #EFE;
   }
   *, ::after, ::before {
         box-sizing: inherit;
@@ -13,5 +15,5 @@ export const GlobalStyles = createGlobalStyle`
     font-size: 16px; 
     display: flex;
     flex-direction: column;
-    align-items: center;
+    justify-content: center;
 }`
