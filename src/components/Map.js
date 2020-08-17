a5b5b2ea2c91c78e208db05d1c46d1c2ae189f14
Tomasz Sypko <tsypko@gmail.com>
2020-08-18 00:08:40 +0200

    Map component created

---
+++ src/components/Map.js
@@ -0,0 +1,32 @@
+import React from "react";
+import {Map, GoogleApiWrapper} from 'google-maps-react';
+
+const style = {
+    width: "100%",
+    height: "400px"
+    }
+    const containerStyle = {
+        position: 'relative',  
+        width: '100%',
+        height: '100%'
+      }
+const MapElement = () => (
+    <div>
+        <Map 
+        containerStyle={containerStyle} 
+        google={window.google} 
+        style={style}
+        initialCenter={{
+            lat: 50.037464,
+            lng: 22.004763
+          }}
+          zoom={15}
+        />
+    </div>
+)
+
+
+
+export default GoogleApiWrapper({
+    apiKey: ("AIzaSyBmWH2FzxWyJ23vsnG9bnbZXcnPqQ7qQns")
+   })(MapElement);
