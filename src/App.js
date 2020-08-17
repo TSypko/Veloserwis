fb8b5d45512d81a059d3f2231303fd3ef4bca9f8
Tomasz Sypko <tsypko@gmail.com>
2020-08-17 18:40:25 +0200

    added navigation components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { GlobalStyles } from './GlobalStyles';
+import Wrapper from "./components/Wrapper"
+import LogoContainer from "./components/LogoContainer/index"
+import Navigation from "./components/Navigation"
+import NavigationMenu from "./components/NavigationMenu"
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <div>
-        Hello world
-    </div>
+      <Wrapper>
+      <Navigation>
+        <LogoContainer />
+        <NavigationMenu />
+      </Navigation>
+      </Wrapper>
     </>
   );
 }
