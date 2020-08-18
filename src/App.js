2017c4020cda2d3f8124a3cb479f9493358c1cf8
Tomasz Sypko <tsypko@gmail.com>
2020-08-18 20:54:00 +0200

    added document title & description from API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import React, { useState } from 'react';
 import { GlobalStyles } from './GlobalStyles';
-import Wrapper from "./components/Wrapper"
-import LogoContainer from "./components/LogoContainer/index"
-import Navigation from "./components/Navigation"
-import NavigationMenu from "./components/NavigationMenu"
-import Main from "./components/Main"
-import Section from "./components/Section"
-import Hero from "./components/Hero"
-import About from "./components/About"
-import Pricing from "./components/Pricing"
-import Contact from "./components/Contact"
-import Footer from "./components/Footer"
-import SocialInfo from "./components/SocialInfo"
-import BurgerButton from "./components/Burger"
+import Wrapper from "./components/Wrapper";
+import LogoContainer from "./components/LogoContainer/index";
+import Navigation from "./components/Navigation";
+import NavigationMenu from "./components/NavigationMenu";
+import Main from "./components/Main";
+import Section from "./components/Section";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Pricing from "./components/Pricing";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import SocialInfo from "./components/SocialInfo";
+import BurgerButton from "./components/Burger";
+import { useFetch } from "./useFetch";
 
 function App() {
   const [isOpened, setIsOpened] = useState(false);
 
-    const openMenu = () => {
-        setIsOpened(!isOpened);
-    };
+  const openMenu = () => {
+    setIsOpened(!isOpened);
+  };
+
+  const wpMainData = useFetch("http://localhost/RestAPITest/wp-json/");
+
+  const setDocumentHead = () => {
+    console.log(wpMainData);
+    if (wpMainData.response) {
+       document.title = wpMainData.response.name; 
+       document.head.querySelector('meta[name="description"]').content = wpMainData.response.description;
+      };
+  };
+  setDocumentHead();
 
   return (
     <>
