51277504f00f0026a732c6562b68622371b87375
Tomasz Sypko <tsypko@gmail.com>
2020-08-19 19:00:35 +0200

    Updates

---
+++ precache-manifest.54d0ea9ebde226f499c13a30213226dd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "985200f52b7e751c79ed24aa823127e0",
+    "url": "/Veloserwis/index.html"
+  },
+  {
+    "revision": "e8aaf5982ee08be29a9d",
+    "url": "/Veloserwis/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "775c6259096e9c8f7409",
+    "url": "/Veloserwis/static/js/2.d7ac10c7.chunk.js"
+  },
+  {
+    "revision": "297066dc9c240cac50b6991385e4046c",
+    "url": "/Veloserwis/static/js/2.d7ac10c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8aaf5982ee08be29a9d",
+    "url": "/Veloserwis/static/js/main.afa0ba25.chunk.js"
+  },
+  {
+    "revision": "8d777d9455a33ec4e27f",
+    "url": "/Veloserwis/static/js/runtime-main.d1816137.js"
+  },
+  {
+    "revision": "c802df8bc06a17fd3618489fdc26b646",
+    "url": "/Veloserwis/static/media/background.c802df8b.jpg"
+  },
+  {
+    "revision": "b035343f5faaa6f2435d16b64a8a5ffe",
+    "url": "/Veloserwis/static/media/logo.b035343f.svg"
+  },
+  {
+    "revision": "78896c141aa99452781ee9db79359492",
+    "url": "/Veloserwis/static/media/pricing.78896c14.jpg"
+  }
+]);
