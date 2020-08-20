7bf8a767bcbc2cd456af7c271a1ebde462e7a58b
Tomasz Sypko <tsypko@gmail.com>
2020-08-20 20:59:44 +0200

    name changed

--- src/useShowOnScrollX.js
+++ src/useShowOnScrollX.js
@@ -4,14 +4,14 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
-export const useShowOnXScroll = (ref, amount, startRef, startPos) => {
-useEffect(() => {
-    gsap.fromTo(ref, { x: amount, autoAlpha: 0 }, {
-        x: 0, autoAlpha: 1, duration: 1, ease: 'easeInOut', scrollTrigger: {
-            trigger: startRef,
-            start: startPos,
-        }
-    });
-}, [ref, amount, startRef, startPos]);
+export const useShowOnScrollX = (ref, amount, startRef, startPos) => {
+    useEffect(() => {
+        gsap.fromTo(ref, { x: amount, autoAlpha: 0 }, {
+            x: 0, autoAlpha: 1, duration: 1, ease: 'easeInOut', scrollTrigger: {
+                trigger: startRef,
+                start: startPos,
+            }
+        });
+    }, [ref, amount, startRef, startPos]);
 
 };
--- src/useShowOnScrollY.js
+++ src/useShowOnScrollY.js
@@ -4,14 +4,14 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
-export const useShowOnYScroll = (ref, amount, startRef, startPos) => {
-useEffect(() => {
-    gsap.fromTo(ref, { y: amount, autoAlpha: 0 }, {
-        y: 0, autoAlpha: 1, duration: 1, ease: 'easeInOut', scrollTrigger: {
-            trigger: startRef,
-            start: startPos,
-        }
-    });
-}, [ref, amount, startRef, startPos]);
+export const useShowOnScrollY = (ref, amount, startRef, startPos) => {
+    useEffect(() => {
+        gsap.fromTo(ref, { y: amount, autoAlpha: 0 }, {
+            y: 0, autoAlpha: 1, duration: 1, ease: 'easeInOut', scrollTrigger: {
+                trigger: startRef,
+                start: startPos,
+            }
+        });
+    }, [ref, amount, startRef, startPos]);
 
 };
