e848f1456b2c9187448d5ecf0bf017d2b1162f3b
Tomasz Sypko <tsypko@gmail.com>
2020-08-22 16:48:34 +0200

    google-react-maps removed

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,7 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-import { Map, GoogleApiWrapper } from 'google-maps-react';
-
 const MapContainer = styled.div`
     width: 100%;
     height: 400px;
@@ -16,28 +14,11 @@ const style = {
     width: "100%",
     height: "100%"
 }
-const containerStyle = {
-    position: 'relative',
-    width: '100%',
-    height: '100%'
-}
+
 const MapElement = () => (
     <MapContainer>
-        <Map
-            containerStyle={containerStyle}
-            google={window.google}
-            style={style}
-            initialCenter={{
-                lat: 50.037464,
-                lng: 22.004763
-            }}
-            zoom={15}
-        />
+       <iframe title="Veloserwis" src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d1077.4515871474516!2d22.004156287201862!3d50.03709998915204!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1spl!2spl!4v1598107538268!5m2!1spl!2spl" frameBorder="0" style={style} allowFullScreen="" aria-hidden="false" tabIndex="0"></iframe>
     </MapContainer>
 )
 
-
-
-export default GoogleApiWrapper({
-    apiKey: ("AIzaSyBmWH2FzxWyJ23vsnG9bnbZXcnPqQ7qQns")
-})(MapElement);
+export default MapElement;
